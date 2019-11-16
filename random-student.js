5471ff13674545d215de048278b3fea991026927
Marque Hudspeth <marquelamar@gmail.com>
2019-11-15 21:04:24 -0600

    Create Array of student names

---
+++ random-student.js
@@ -0,0 +1,13 @@
+/* add an array
+add everyones name in it
+ramdomly pick somoene name
+ */
+const myArr = ["Andrew","Billy","George","Heather","Madison","Marque","Martin","Nikki","Oscar","RhondaLyn","Sidney","Tracie","Wallat"];
+
+console.log(myArr[10].length);//itsa property because it does not have ()
+
+const studnetsObj = {
+  "0": "test" //you cannot use bracket notation when you use a string as a key?
+}
+
+
